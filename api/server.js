d08c55738522e74d296a1951de6b75abd518131b
Luxadin <9556768+Luxadin@users.noreply.github.com>
2022-09-13 17:05:41 -0700

    heroku port fix

--- api/server.js
+++ api/server.js
@@ -8,6 +8,7 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
+// connect to the database
 mongoose.connect("mongodb+srv://pizza:pizza@cluster0.tgoasdj.mongodb.net/?authMechanism=DEFAULT", {
     useNewUrlParser: true,
     useUnifiedTopology: true
@@ -16,9 +17,12 @@ mongoose.connect("mongodb+srv://pizza:pizza@cluster0.tgoasdj.mongodb.net/?authMe
     .catch(console.error);
 
 
+// redirects requests back to index.html
+
 app.use(express.static(path.join(__dirname, '..', 'client', 'build')));
 app.get('/', (req, res) =>{
     res.sendFile(path.join(__dirname, '..', 'client', 'build', 'index.html'));
 })
 
-app.listen(3000, () => console.log("Server started on port 3000"));
+const port = process.env.PORT || 3000
+app.listen(port, () => console.log("Server started on port ${port}..."));
